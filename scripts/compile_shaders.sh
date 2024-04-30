511943fe9cd3b014c0b8ccea6d8f1c72a67f273c
Adrien Bennadji <adrien.bennadji@live.fr>
2024-04-30 13:08:12 +0200

    recompile shaders targeting spirv1.6

--- scripts/compile_shaders.sh
+++ scripts/compile_shaders.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-find ./crates/examples/*/shaders -not -name *.spv -type f -exec glslangValidator --target-env spirv1.4 -V -o {}.spv {} \;
+find ./crates/examples/*/shaders -not -name *.spv -type f -exec glslangValidator --target-env spirv1.6 -V -o {}.spv {} \;
